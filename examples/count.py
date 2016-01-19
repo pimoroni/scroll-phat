273c140436e86a3b80b2e1862ccb6b17b19a1246
AndiCui <andy990525@me.com>
2016-01-19 10:29:02 +0100

    Put str to print in () [Python3 Compatibility]

--- examples/count.py
+++ examples/count.py
@@ -5,12 +5,12 @@ import sys
 import time
 
 if(len(sys.argv) == 1):
-    print "Type a number in under 999 as an argument."
+    print("Type a number in under 999 as an argument.")
     sys.exit(-1)
 val = int(sys.argv[1])
 
 if(val > 999):
-    print "Number must be under 999 to fit on screen"
+    print("Number must be under 999 to fit on screen")
     sys.exit(-1)
 
 scrollphat.set_brightness(7)
