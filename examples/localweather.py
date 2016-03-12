7b620a66a47062d203ffc96f81e56eccf67c0e24
Alex Ellis <alexellis2@gmail.com>
2016-03-12 09:17:42 +0000

    Local weather example builds on ukweather.py, enabling users of scrollphat to access the yahoo weather API providing global data, not just the UK. Uses IP-address based location lookup as per ip.py.
    Gives summary for today/tomorrow in international unit (F).

--- examples/localweather.py
+++ examples/localweather.py
@@ -18,23 +18,57 @@ def get_location():
         return json_data
     return {}
 
-def get_weather(city,country):
+# Python 2 vs 3 breaking changes.
+def encode(qs):
+    val = ""
+    try:
+        val = urllib.urlencode(qs).replace("+","%20")
+    except:
+        val = urllib.parse.urlencode(qs).replace("+", "%20")
+    return val
+
+def get_weather(address):
     base = "https://query.yahooapis.com/v1/public/yql?"
-    query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\""+city+", "+country+"\")"
+    query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\""+address+"\")"
     qs={"q": query, "format": "json", "env": "store://datatables.org/alltableswithkeys"}
-    uri = base +  urllib.urlencode(qs).replace("+","%20")
+
+    uri = base + encode(qs)                                        
+
     res = requests.get(uri)
     if(res.status_code==200):
         json_data = json.loads(res.text)
         return json_data
     return {}
 
-location = get_location()
-if(location["city"]!=None):
-    results = get_weather(location["city"], location["country"])
-#    print json.dumps( results )
-    for x in range(0, 2):
-#	{u'code': u'24', u'text': u'Partly Cloudy/Wind', u'high': u'42', u'low': u'34', u'date': u'2 Mar 2016', u'day': u'Wed'}
-        item = results["query"]["results"]["channel"]["item"]["forecast"][x]
-        print( item["day"] +": " + item["text"] + " - L: "+ item["low"] + "F - H: "+ item["high"]+"F" )
+def scroll_message(output):
+    scrollphat.write_string(output)
+    scrollphat.update()
 
+    while(True):
+        try:
+            scrollphat.scroll()
+            scrollphat.update()
+            time.sleep(0.2)
+        except KeyboardInterrupt:
+            return
+
+if(__name__ == '__main__'):
+    scrollphat.set_brightness(4)
+    location = get_location()
+    location_string = location["city"] +", " + location["country"]
+    print("Location: " + location_string)
+
+    if(location["city"] != None):
+        weather = get_weather(location_string)
+        output = ""
+
+        # Feel free to pick out other data here, for the scrolling message
+        for x in range(0, 2):
+            item = weather["query"]["results"]["channel"]["item"]["forecast"][x]
+            output = output + item["day"] +": " + item["text"] + " - L: "+ item["low"] + "F - H: "+ item["high"]+"F"
+
+        print(output)
+        scroll_message(output)
+
+        scrollphat.clear()
+        quit()
