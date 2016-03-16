53a047c38c6e7070f96379ff4bcb5905292e7f80
AndiCui <andy990525@me.com>
2016-03-16 19:26:28 +0100

    Countdown Timer Example

---
+++ examples/countdown.py
@@ -0,0 +1,45 @@
+import datetime
+import time
+import scrollphat
+
+try:
+    print("Please enter the target time with intergers")
+    year = input("Year:\t")
+    month = input("Month:\t")
+    d = input("Date:\t")
+    h = input("Hour:\t")
+    m = input("Minute:\t")
+
+
+    while 1:
+        delta = datetime.datetime(int(year), int(month), int(d), int(h), int(m)) - datetime.datetime.now()
+        deltaSec = int(delta.total_seconds())
+        deltaMin = int(delta.total_seconds() // 60)
+        deltaHour = int(delta.total_seconds() // 3600)
+
+        if deltaMin > 99 and deltaHour < 99:
+            dis = str(deltaHour) + "h"
+            scrollphat.write_string(dis)
+            time.sleep(450)
+            scrollphat.clear()
+        elif deltaSec > 999 and deltaMin <= 99:
+            dis = str(deltaMin) + "\""
+            scrollphat.write_string(dis)
+            time.sleep(15)
+            scrollphat.clear()
+        elif 0 <= deltaSec <= 999:
+            dis = str(deltaSec)
+            scrollphat.write_string(dis)
+            time.sleep(0.25)
+            scrollphat.clear()
+        elif deltaSec < 0:
+            for x in range(11):
+                for y in range(5):
+                    scrollphat.set_pixel(x,y,1)
+            scrollphat.update()
+        else:
+            print("Cannot display: Out of range.")
+            scrollphat.clear()
+            exit()
+except KeyboardInterrupt:
+    scrollphat.clear()
