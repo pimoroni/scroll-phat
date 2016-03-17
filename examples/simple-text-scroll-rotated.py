ce4fba92462264126154f80040e098f0009ac24e
Phil Howard <phil@gadgetoid.com>
2016-03-17 18:06:17 +0000

    Uses rotate method

--- examples/simple-text-scroll-rotated.py
+++ examples/simple-text-scroll-rotated.py
@@ -8,8 +8,8 @@ if len(sys.argv) != 2:
     print("\nusage: python simple-text-scroll-rotated.py \"message\" \npress CTRL-C to exit\n")
     sys.exit(0)
 
+scrollphat.set_rotate(True)
 scrollphat.write_string(sys.argv[1] + "   ")
-scrollphat.rotate = True
 
 while True:
     try:
