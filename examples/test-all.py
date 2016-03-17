4eb775e5a0a166bf91aa09b66207acfe9f7c2f97
Phil Howard <phil@gadgetoid.com>
2016-03-17 18:06:32 +0000

    Test patterns, example of set_pixels

--- examples/test-all.py
+++ examples/test-all.py
@@ -10,18 +10,29 @@ def millis():
 
 scrollphat.set_brightness(2)
 
+def set_checker(offset):
+    n = offset
+    for y in range(5):
+        for x in range(11):
+            scrollphat.set_pixel(x,y,n % 2 == 0)
+            n += 1
+    scrollphat.update()
+
 while True:
     try:
-        for x in range(11):
-            for y in range(5):
-                scrollphat.set_pixel(x,y,1)
-                scrollphat.update()
-                time.sleep(0.03)
-        for x in range(11):
-            for y in range(5):
-                scrollphat.set_pixel(x,y,0)
-                scrollphat.update()
-                time.sleep(0.03)
+        scrollphat.set_pixels(lambda x, y: 1, auto_update=True)
+        time.sleep(0.5)
+
+        scrollphat.set_pixels(lambda x, y: y % 2 == 0, auto_update=True)
+        time.sleep(0.5)
+
+        scrollphat.set_pixels(lambda x, y: x % 2 == 0, auto_update=True) 
+        time.sleep(0.5)
+
+        set_checker(0)
+        time.sleep(0.5)
+        set_checker(1)
+        time.sleep(0.5)
     except KeyboardInterrupt:
         scrollphat.clear()
         sys.exit(-1)
