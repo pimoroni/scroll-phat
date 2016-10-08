43428b602b2bd7f11ba4ffae07a91fc6f455e1d1
RogueM <hal@pimoroni.com>
2016-10-08 13:57:42 +0000

    test-alt script

--- examples/test-alt.py
+++ examples/test-alt.py
@@ -6,28 +6,29 @@ import time
 
 import scrollphat
 
+
 def millis():
     return int(round(time.time() * 1000))
 
-scrollphat.set_brightness(2)
+scrollphat.alt_set_brightness(2)
 
 def set_checker(offset):
     n = offset
     for y in range(5):
         for x in range(11):
-            scrollphat.set_pixel(x,y,n % 2 == 0)
+            scrollphat.alt_set_pixel(x,y,n % 2 == 0)
             n += 1
-    scrollphat.update()
+    scrollphat.alt_update()
 
 while True:
     try:
-        scrollphat.set_pixels(lambda x, y: 1, auto_update=True)
+        scrollphat.alt_set_pixels(lambda x, y: 1, auto_update=True)
         time.sleep(0.5)
 
-        scrollphat.set_pixels(lambda x, y: y % 2 == 0, auto_update=True)
+        scrollphat.alt_set_pixels(lambda x, y: y % 2 == 0, auto_update=True)
         time.sleep(0.5)
 
-        scrollphat.set_pixels(lambda x, y: x % 2 == 0, auto_update=True) 
+        scrollphat.alt_set_pixels(lambda x, y: x % 2 == 0, auto_update=True) 
         time.sleep(0.5)
 
         set_checker(0)
@@ -35,5 +36,5 @@ while True:
         set_checker(1)
         time.sleep(0.5)
     except KeyboardInterrupt:
-        scrollphat.clear()
+        scrollphat.alt_clear()
         sys.exit(-1)
