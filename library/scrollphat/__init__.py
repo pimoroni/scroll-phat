e4aa4dcbdd82b0ab8786ba78630f3a169f00bd79
RogueM <chaosgen6@googlemail.com>
2016-10-08 13:56:08 +0100

    trying to support altbus

--- library/scrollphat/__init__.py
+++ library/scrollphat/__init__.py
@@ -1,12 +1,34 @@
 from .i2c_bus import bus
+try:
+    from .i2c_bus import altbus
+except ImportError:
+    pass
+
 from .font import font
 from .IS31FL3730 import IS31FL3730
 
+mainbus = bus
+
+try:
+    controller = IS31FL3730(font, bus)
+except IOError:
+    mainbus = None
+
+if mainbus != None:
+    try:
+        altcontroller = IS31FL3730(font, altbus)
+except (NameError, IOError):
+        pass
+else:
+    try:
+        controller = IS31FL3730(font, altbus)
+except (NameError, IOError):
+        exit("Scroll pHAT can't be detected!")
+        
 
 ROTATE_OFF = False
 ROTATE_180 = True
 
-controller = IS31FL3730(font, bus)
 
 def set_rotate(value):
     """Set the rotation of Scroll pHAT
