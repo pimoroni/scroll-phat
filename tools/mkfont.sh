8782a82bf81d84b55ff3a9b387570da9811bffc7
RogueM <chaosgen6@googlemail.com>
2017-04-05 19:31:04 +0000

    removes byte code files if present

--- tools/mkfont.sh
+++ tools/mkfont.sh
@@ -2,6 +2,7 @@
 
 python ./mkfont.py > ./font.py
 
-sudo find /usr -name font.py | grep "scrollphat" | xargs -n 1 sudo cp ./font.py
+sudo find /usr -name font.py | grep -w "scrollphat" | xargs -n 1 sudo cp ./font.py
+sudo find /usr -name *.pyc | grep -w "scrollphat" | xargs sudo rm &> /dev/null
 
 exit 0
