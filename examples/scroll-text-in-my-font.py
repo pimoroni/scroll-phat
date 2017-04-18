0a3233155bd9c3553275448dfa32cc3d52187a46
DW-42 <davidwright4350@gmail.com>
2017-04-18 21:50:05 +1000

    Add files via upload

--- examples/scroll-text-in-my-font.py
+++ examples/scroll-text-in-my-font.py
@@ -4,20 +4,32 @@ import sys
 import time
 import scrollphat
 
+#-----------------------------------------------------------------------------
+# This example use a custome font set, read from
+# the image file my-font, which has the lower-case
+# letters replaced with symbols.
+# For example:
+#   a - tick
+#   b - cross
+#   f - flag
+#   h - love heart 
+#   j - smiley face
+#   u,d,l,r - arrows (up down left and right respectively)
+#-------------------------------------------------------------------------------
+
 try:
     from PIL import Image
 except ImportError:
     exit("This script requires the pillow module\nInstall with: sudo pip install pillow")
 
 #-----------------------------------------------------------------------------
-# This funtion convert the supplied image into font
-# data for Scool pHAT. The expect format of the
-# image is as follows: Each font image contains
-#a 16 x 4 table of squares, one for each ASCII 
-# character, starting with a space (0x20) and 
-# incrementing from left to right. Each square 
-# is 6x6 box boarding the 5x5 image of the 
-# individual characters.
+# This funtion will read the supplied image 
+# and convert it into font data for Scoll pHAT . 
+# The expect format of the image is as follows: 
+# Each font image contains a 16 x 6 table of squares, 
+# one for each ASCII character, starting with a space (0x20) and 
+#  incrementing from left to right. Each square is 6x6 box 
+# boarding the 5x5 image of the individual characters. 
 def convert_png2font(font_file):
 
     font = {}
@@ -26,7 +38,7 @@ def convert_png2font(font_file):
 
     char = 0x20
     gridsize_x = 16
-    gridsize_y = 4
+    gridsize_y = 6
     border = 1
     for char_y in range(0, gridsize_y):
         for char_x in range(0, gridsize_x):
@@ -39,11 +51,11 @@ def convert_png2font(font_file):
                         bits |= (1 << y)
                 char_bits.append(bits)
 
-            # remove all "empty" columns from end of character
-                while len(char_bits) > 0 and char_bits[-1] == 0:
-                    char_bits.pop()
+            # remove all "empty" columns from the right of the character
+            while len(char_bits) > 0 and char_bits[-1] == 0:
+                char_bits.pop()
 
-                font[char] = char_bits
+            font[char] = char_bits
 
             char += 1
     return font
@@ -58,7 +70,10 @@ scrollphat.set_rotate(True)
 scrollphat.load_font(convert_png2font('my-font.png'))
 
 if len(sys.argv) != 2:
-    print("\nusage: python scroll-text-in-my-font.py \"message\" \npress CTRL-C to exit\n")
+    print("\nUsage: python scroll-text-in-my-font.py \"MESSAGE\" " )
+    print ("press CTRL-C to exit\n")
+    print("In this example the lettters are read from the \"my-font.png\" file,")
+    print("where lower-case letters are replaced with fun symbols and icons.")    
     sys.exit(0)
 
 scrollphat.write_string(sys.argv[1], 11)
